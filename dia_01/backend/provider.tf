df393f5e4d2baee6e85d6308d31d7c6ae6094db7
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-03-20 03:46:18 -0300

    Backend Remoto

--- dia_01/backend/provider.tf
+++ dia_01/backend/provider.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "descomplicando-terraform-vasco"
+    key    = "aula_backend"
+    region = "us-east-2"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
