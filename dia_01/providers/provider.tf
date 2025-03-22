8b720a1af7983b3829782b72ea3adc26fd7fb3b1
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-03-21 22:54:39 -0300

    Aulas de providers e variables

---
+++ dia_01/providers/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+
+# Additional provider configuration for west coast region; resources can
+# reference this as `aws.west`.
+provider "aws" {
+  alias  = "west"
+  region = "us-west-2"
+}
